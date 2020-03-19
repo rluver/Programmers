3038a3313aa1cce371c180f3adf5edd2400ecc26
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-03-19 14:17:40 +0900

    Update sort #1

--- Challenges.py
+++ Challenges.py
@@ -1,3 +1,20 @@
+# Sort
+## 1
+def solution(array, commands):
+
+    answer = []
+    temp = 0
+    
+    for i in range(len(commands)):
+        temp = array[commands[i][0] - 1:commands[i][1]]
+        temp.sort()
+        answer.append(temp[commands[i][2] - 1])
+    
+    return answer
+
+
+
+
 # Heap
 ## 1
 def unequal(x):
