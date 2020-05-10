e9190e8be02419a28577979d2bd4bb6a9fe5725f
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-05-10 23:20:50 +0900

    add: Edit Challenges.cpp to include stack/queue #3

--- Challenges.cpp
+++ Challenges.cpp
@@ -22,3 +22,74 @@ vector<int> solution(vector<int> heights)
 
     return answer;
 }
+
+
+## 3
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds)
+{
+    double epsilon = 1e-10;
+    int i, cnt;
+    vector<int> time, answer;
+    vector<double> diff;
+
+    for (i = 0; i < (int)progresses.size(); i++)
+    {
+        diff.push_back((double)(100 - progresses[i]) / (double)speeds[i]);
+    }
+        
+    for (i = 0; i < (int)diff.size(); i++)
+    {
+        if (diff[i] - (int)diff[i] < epsilon)
+        {
+            time.push_back(int(diff[i]));
+        }
+        else
+        {
+            time.push_back(int(diff[i] + 1));
+        }
+    }
+   
+    for (i = 0; i < (int)time.size() - 1; i++)
+    {
+        if (time[i] > time[i + 1])
+        {
+            time[i + 1] = time[i];
+        }
+    }
+        
+    cnt = 1;
+    for (i = 0; i < (int)time.size(); i++)
+    {
+        if (i < (int)time.size() - 1) 
+        {
+            if (time[i] == time[i + 1])
+            {
+                cnt++;
+            }
+            else
+            {
+                answer.push_back(cnt);
+                cnt = 1;
+            }
+        }
+        else
+        {
+            if (time[i] == time[i - 1])
+            {
+                answer.push_back(cnt);
+            }
+            else
+            {
+                answer.push_back(1);
+            }
+        }
+    }
+
+    return answer;    
+}
