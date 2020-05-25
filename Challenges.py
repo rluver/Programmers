6d2d599ea236c528eaab80047fa789e3f69bc7b7
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-05-25 18:15:54 +0900

    add: Edit Challenges.py to include greedy #4

--- Challenges.py
+++ Challenges.py
@@ -343,3 +343,25 @@ def solution(n, lost, reserve):
     answer = sum(list(map(lambda x: x >= 1, vec)))
     
     return answer
+
+
+## 4
+def solution(people, limit):
+    
+    people.sort()
+    
+    start = 0
+    end = len(people) - 1
+    dup = 0
+    
+    while start < end:        
+        if people[start] + people[end] <= limit:
+            dup += 1
+            start += 1
+            end -= 1
+        else:
+            end -= 1
+     
+    answer = len(people) - dup
+    
+    return answer
