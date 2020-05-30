0ec653259322889d52067fe0a505609e920a64c2
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-05-30 20:45:53 +0900

    add: Edit Challenges.py to include stack/queue #6

--- Challenges.py
+++ Challenges.py
@@ -129,6 +129,22 @@ def solution(progresses, speeds):
     return answer
 
 
+# 6
+def solution(prices):
+    
+    answer = []
+        
+    for i in range(len(prices)):
+        time = 0
+        for j in range(i + 1, len(prices)):            
+            time += 1
+            if prices[i] > prices[j]:
+                break
+        answer.append(time)
+                        
+    return answer
+
+
 
 
 # Heap
