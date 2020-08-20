35e595fbbf1475fe6e094c55492f9a1095e6dea8
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-08-20 22:04:24 +0900

    add: Edit Challenges.py to include brute-force #3

--- Challenges.py
+++ Challenges.py
@@ -356,6 +356,16 @@ def solutions(numbers):
     
     answer = len(permutation)
     
+    
+## 3
+
+def solution(brown, yellow):
+    
+    x = (brown + 4 + ((brown + 4) ** 2 - 16 * (brown + yellow)) ** 0.5) / 4
+    y = (brown + yellow) // x
+   
+    return [max(x, y), min(x, y)]
+    
 
 
 
